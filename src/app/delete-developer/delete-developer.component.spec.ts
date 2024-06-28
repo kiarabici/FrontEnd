9e1737c003d63bf7015b57708e61c8f5e51cf68e
Kiara Bici <kiara.bici16@gmail.com>
2024-06-28 13:45:53 +0200

    sec commit

---
+++ src/app/delete-developer/delete-developer.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DeleteDeveloperComponent } from './delete-developer.component';
+
+describe('DeleteDeveloperComponent', () => {
+  let component: DeleteDeveloperComponent;
+  let fixture: ComponentFixture<DeleteDeveloperComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DeleteDeveloperComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DeleteDeveloperComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
